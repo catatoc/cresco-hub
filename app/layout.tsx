636f500bd1e6716fbaf8ad2336cf0188a17a4ec3
Carlos Horacio Carrasquero <77286510+catatoc@users.noreply.github.com>
2026-06-10 17:47:53 +0800

    fix(portal): 🔤 align site meta description with new login copy

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "crescō",
-  description: "Tu proyecto. Sin abrir Notion.",
+  description: "Crecemos contigo.",
 };
 
 export default function RootLayout({
